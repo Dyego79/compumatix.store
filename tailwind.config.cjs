16ff5ffcb0f0a49e2bde629404d54287713e5777
Diego Perez <dyego79@gmail.com>
2025-02-27 19:30:03 -0300

    inicio

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,ts,tsx,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ["Lexend Variable", "sans-serif"],
+      },
+    },
+  },
+  // ... resto de config
+};
